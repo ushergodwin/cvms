ddd305da0c0fc9929a0eaa2f4383c05b10ba19c0
= <=>
2022-01-18 01:49:25 +0300

    create pdf v_card, download v_card

--- covidvms/static/assets/cvmsjs/cvms.js
+++ covidvms/static/assets/cvmsjs/cvms.js
@@ -1,4 +1,18 @@
 $(document).ready(function (){
+    $.fn.extend({
+        printSection: function() {
+            var frameName = 'printIframe';
+            var doc = window.frames[frameName];
+            if (!doc) {
+                $('<iframe>').hide().attr('name', frameName).appendTo(document.body);
+                doc = window.frames[frameName];
+            }
+            doc.document.body.innerHTML = this.html();
+            doc.window.print();
+            return this;
+        }
+    });
+    
     $("#loginForm").on('submit', function(e){
         e.preventDefault()
         const url = $(this).attr('action')
